0c624a1795bc428774216c3cdacbbf9acb780a9a
@hexlet/cli <support@hexlet.io>
2023-07-24 21:20:53 +0500

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -11,9 +11,9 @@ public class App {
 
         String[] letters = symbols.split("");
 
-        List<String> array = new Arrays.asList(letters);
+        List<String> array = new ArrayList(Arrays.asList(letters));
 
-        for (var i = 0; i < word.length; i++) {
+        for (var i = 0; i < word.length(); i++) {
             String current = word.substring(i, i + 1).toLowerCase();
 
             if (!array.contains(current)) {
